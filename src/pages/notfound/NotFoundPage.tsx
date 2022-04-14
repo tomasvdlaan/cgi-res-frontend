ef9866de6b50cf2ea13e0fa959697c84d57f6f17
alexxtodea <485032@student.fontys.nl>
2022-04-14 20:05:12 +0200

    404 error
    begin aan de error pagina

--- src/pages/notfound/NotFoundPage.tsx
+++ src/pages/notfound/NotFoundPage.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 
 function NotFoundPage() {
@@ -9,5 +10,47 @@ function NotFoundPage() {
 		</div>
 	);
 }
+=======
+
+import React from "react";
+function NotFoundPage() {
+    return (
+        <div className="container">
+        <div className="flex flex-col">
+        <div className="flex flex-col justify-center items-center">
+            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                <div className="sm:text-center lg:text-center">
+                    <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                        <span className="block flex flex-col justify-center items-center">404</span>{' '}
+                        <span className="block text-indigo-600 xl:inline">PAGE NOT FOUND</span>
+                    </h1>
+                </div>
+            </main>
+        
+
+            <div className="flex flex-col justify-center items-center">
+                <button className="btn bg-purple rounded-full flex px-7 py-4 96rem flex flex-col justify-center items-center">Home
+                </button>
+            </div>
+        </div>
+        </div>
+        </div>
+       
+
+
+
+
+
+
+
+
+
+    );
+};
+
+
+
+
+>>>>>>> Stashed changes
 
 export default NotFoundPage;
