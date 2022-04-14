b9074e05b98ef1c071e93fcd1675811e678a2458
alexxtodea <485032@student.fontys.nl>
2022-04-14 23:16:31 +0200

    404 error
    begin aan de error pagina

--- src/pages/notfound/NotFoundPage.tsx
+++ src/pages/notfound/NotFoundPage.tsx
@@ -1,49 +1,34 @@
-<<<<<<< Updated upstream
-import React from "react";
-
-function NotFoundPage() {
-	return (
-		<div>
-			<h1>404</h1>
-			<h2>This is not the page you are looking for</h2>
-			<img src="https://media2.giphy.com/media/UHAYP0FxJOmFBuOiC2/giphy.gif?cid=ecf05e47h993a9r2z92w25d3xeznfb13450gp6v8vih244gp&rid=giphy.gif&ct=g" alt="de"/>
-		</div>
-	);
-}
-=======
 
 import React from "react";
 function NotFoundPage() {
     return (
-        <div className="container">
-        <div className="flex flex-col">
-        <div className="flex flex-col justify-center items-center">
-            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-                <div className="sm:text-center lg:text-center">
-                    <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                        <span className="block flex flex-col justify-center items-center">404</span>{' '}
-                        <span className="block text-indigo-600 xl:inline">PAGE NOT FOUND</span>
-                    </h1>
-                </div>
-            </main>
-        
-
-            <div className="flex flex-col justify-center items-center">
-                <button className="btn bg-purple rounded-full flex px-7 py-4 96rem flex flex-col justify-center items-center">Home
-                </button>
-            </div>
-        </div>
-        </div>
-        </div>
+        <div className="grid bg-light-gray place-items-center h-screen">
+            
+            <img src="/Users/alextodea/Desktop/HIER/cgi-res-frontend/public/Images/404Error.png" alt="Error" />
+            
        
+        
+        <div className="container shadow-xl px-4 py-15 rounded-full">
+                <div className="flex flex-col justify-center items-center">
+                    <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                        <div className="sm:text-center lg:text-center">
+                            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                                <span className="block flex flex-col justify-center items-center text-[55px]">404</span>{' '}
+                                <span className="block p-3 font-medium text-gray text-[30px]">page not found</span>
+                            </h1>
+                        </div>
+                    </main>
 
-
-
-
-
-
-
-
+                    <div className="container px-5 py-7">
+                        <div className="flex flex-col justify-center items-center">
+                            <button className="btn bg-red shadow-xl rounded-full flex px-7 py-4 96rem flex flex-col justify-center items-center">Home
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+    
 
     );
 };
@@ -51,6 +36,5 @@ function NotFoundPage() {
 
 
 
->>>>>>> Stashed changes
 
 export default NotFoundPage;
