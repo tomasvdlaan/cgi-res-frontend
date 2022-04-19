1f67a7478903f27982960e3527f0280c506670ec
Vincent-Maximus <57707853+Vincent-Maximus@users.noreply.github.com>
2022-04-19 14:38:16 +0200

    Fixed EsLint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,31 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+// const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["src/**/*.{ts,js,tsx,jsx}"],
-  theme: {
-    colors: {
-      'red': '#E01937',
-      'gray': '#8E8E93',
-      'light-gray': '#EFEFEF',
-      'white': '#FFFFFF',
-      'purple': '#5236AB',
-      'light-purple': '#EAEAF5',
-    },
-    fontFamily: {
-      'SofiaProBold': ['Sofia Pro Bold', 'sans-serif'],
-      'SofiaProRegular': ['Sofia Pro Regular', 'sans-serif', ...defaultTheme.fontFamily.sans],
-      'SofiaProLight': ['Sofia Pro Light', 'sans-serif'],
-      'body': ['Sofia Pro Bold', 'sans-serif'],
-    },
-    extend: {
+	content: ["src/**/*.{ts,js,tsx,jsx}"],
+	theme: {
+		colors: {
+			"red": "#E01937",
+			"gray": "#8E8E93",
+			"light-gray": "#EFEFEF",
+			"white": "#FFFFFF",
+			"purple": "#5236AB",
+			"light-purple": "#EAEAF5",
+		},
+		fontFamily: {
+			"SofiaProBold": ["Sofia Pro Bold", "sans-serif"],
+			"SofiaProRegular": ["Sofia Pro Regular", "sans-serif"],
+			"SofiaProLight": ["Sofia Pro Light", "sans-serif"],
+			"body": ["Sofia Pro Bold", "sans-serif"],
+		},
+		extend: {
 
-    },
-  },
-  plugins: [
-    function ({ addVariant }) {
-      addVariant("hocus", ["&:focus", "&:hover"]);
-      addVariant("group-hocus", [":merge(.group):focus &", ":merge(.group):hover &"]);
-      addVariant('peer-hocus', [':merge(.peer):focus ~ &', ':merge(.peer):hover ~ &']);
-    }],
-}
+		},
+	},
+	plugins: [
+		function ({ addVariant }) {
+			addVariant("hocus", ["&:focus", "&:hover"]);
+			addVariant("group-hocus", [":merge(.group):focus &", ":merge(.group):hover &"]);
+			addVariant("peer-hocus", [":merge(.peer):focus ~ &", ":merge(.peer):hover ~ &"]);
+		}],
+};
 
