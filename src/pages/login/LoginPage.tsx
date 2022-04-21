d5c309a4766699919ab0138a7841b26fe46ee984
Robin Mager <robinmager@home.nl>
2022-04-21 15:24:06 +0200

    Update LoginPage.tsx

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import Datepicker from "../../glob-components/Datepicker";
 
 function LoginPage() {
-	const startDate = new Date();
-	const endDate = new Date(startDate);
-	endDate.setMonth(endDate.getMonth() + 3);
-
-	return (
-		<Datepicker onPick={(date) => console.log(date)} startDate={startDate} endDate={endDate} />
-	);
+	return (<div></div>);
 }
 
 export default LoginPage;
