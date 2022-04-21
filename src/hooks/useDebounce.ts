a845e2046ce114603ebfee45628aab0e6cea2470
Robin Mager <robinmager@home.nl>
2022-04-21 20:01:00 +0200

    Fixed linting

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,7 +1,10 @@
 // Disabled because this should be able to take *any* function
-// eslint-disable-next-line @typescript-eslint/ban-types
+// eslint-disable-next-line
 function useDebounce(this: any, fn: Function, timeout: number) {
 	let timer: NodeJS.Timeout;
+
+	// Disabled because this should be able to take any argument
+	// eslint-disable-next-line
 	return (...args: any) => {
 		clearTimeout(timer);
 		timer = setTimeout(() => { fn.apply(this, args); }, timeout);
