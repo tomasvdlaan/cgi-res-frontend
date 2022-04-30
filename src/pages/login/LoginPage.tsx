a41707d1546ea14f36e9711dd61fa6fddd25c87e
Robin Mager <robinmager@home.nl>
2022-04-30 14:37:18 +0200

    Added timepicker and useRange hook

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import DatePicker from "../../glob-components/DatePicker";
+import TimePicker from "../reservations/create/components/TimePicker";
 
 function LoginPage() {
 	const startDate = new Date();
 	const endDate = new Date(startDate);
 	endDate.setMonth(endDate.getMonth() + 3);
 
+	/*	return (
+		/!*<DatePicker onPick={(date) => console.log(date)} startDate={startDate} endDate={endDate} />*!/
+	);*/
+
 	return (
-		<DatePicker onPick={(date) => console.log(date)} startDate={startDate} endDate={endDate} />
+		<TimePicker onPick={time => console.log(time)} />
 	);
 }
 
